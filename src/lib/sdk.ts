5fa4ec4c400ec88820912eb8dfd4744c90d3c520
Andreas Oee <andy.oeee@gmail.com>
2025-04-30 14:19:25 +0700

    chore: upgrate sdk

--- src/lib/sdk.ts
+++ src/lib/sdk.ts
@@ -4,6 +4,5 @@ const devOrigin = "http://localhost:3001/";
 const prodOrigin = "https://yodl.me/";
 
 export const sdk = new YappSDK({
-  ensName: "deposit-yapp.yodl.eth",
   origin: process.env.NODE_ENV === "development" ? devOrigin : prodOrigin,
 });
